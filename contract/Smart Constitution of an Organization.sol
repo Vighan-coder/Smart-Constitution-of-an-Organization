fd73531f0780460fe1ead8694d790a2d98ed282b
Vighan-coder <vighnrajverma00893@gmail.com>
2025-04-23 15:52:59 +0530

    Update Smart Constitution of an Organization.sol

--- contract/Smart Constitution of an Organization.sol
+++ contract/Smart Constitution of an Organization.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SmartConstitution {
+    address public owner;
+    string public constitution;
+    mapping(address => bool) public members;
+
+    event ConstitutionUpdated(string newConstitution);
+    event MemberAdded(address member);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action.");
+        _;
+    }
+
+    constructor(string memory initialConstitution) {
+        owner = msg.sender;
+        constitution = initialConstitution;
+        members[msg.sender] = true;
+    }
+
+    function updateConstitution(string memory newConstitution) public onlyOwner {
+        constitution = newConstitution;
+        emit ConstitutionUpdated(newConstitution);
+    }
+
+    function addMember(address newMember) public onlyOwner {
+        members[newMember] = true;
+        emit MemberAdded(newMember);
+    }
+}
 
