4a278dd64cb69ceef07bef90b4c514c64ee3eb23
Vighan-coder <vighnrajverma00893@gmail.com>
2025-04-24 19:57:32 +0530

    Update Smart Constitution of an Organization.sol

--- contract/Smart Constitution of an Organization.sol
+++ contract/Smart Constitution of an Organization.sol
@@ -8,6 +8,8 @@ contract SmartConstitution {
 
     event ConstitutionUpdated(string newConstitution);
     event MemberAdded(address member);
+    event MemberRemoved(address member);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can perform this action.");
@@ -26,8 +28,30 @@ contract SmartConstitution {
     }
 
     function addMember(address newMember) public onlyOwner {
+        require(!members[newMember], "Address is already a member.");
         members[newMember] = true;
         emit MemberAdded(newMember);
     }
+
+    function removeMember(address member) public onlyOwner {
+        require(members[member], "Address is not a member.");
+        members[member] = false;
+        emit MemberRemoved(member);
+    }
+
+    function isMember(address addr) public view returns (bool) {
+        return members[addr];
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address.");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
 }
+
 
