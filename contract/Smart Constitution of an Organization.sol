a5399523b58e56979aefae9a965c83fee81c335a
Vighan-coder <vighnrajverma00893@gmail.com>
2025-04-25 20:05:24 +0530

    Update Smart Constitution of an Organization.sol

--- contract/Smart Constitution of an Organization.sol
+++ contract/Smart Constitution of an Organization.sol
@@ -2,56 +2,86 @@
 pragma solidity ^0.8.0;
 
 contract SmartConstitution {
-    address public owner;
-    string public constitution;
-    mapping(address => bool) public members;
+    address public owner;
+    string public constitution;
+    string[] private constitutionHistory;
+    mapping(address => bool) public members;
+    address[] private memberList;
 
-    event ConstitutionUpdated(string newConstitution);
-    event MemberAdded(address member);
-    event MemberRemoved(address member);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event ConstitutionUpdated(string newConstitution);
+    event MemberAdded(address member);
+    event MemberRemoved(address member);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can perform this action.");
-        _;
-    }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action.");
+        _;
+    }
 
-    constructor(string memory initialConstitution) {
-        owner = msg.sender;
-        constitution = initialConstitution;
-        members[msg.sender] = true;
-    }
+    constructor(string memory initialConstitution) {
+        owner = msg.sender;
+        constitution = initialConstitution;
+        constitutionHistory.push(initialConstitution);
+        members[msg.sender] = true;
+        memberList.push(msg.sender);
+    }
 
-    function updateConstitution(string memory newConstitution) public onlyOwner {
-        constitution = newConstitution;
-        emit ConstitutionUpdated(newConstitution);
-    }
+    function updateConstitution(string memory newConstitution) public onlyOwner {
+        constitution = newConstitution;
+        constitutionHistory.push(newConstitution);
+        emit ConstitutionUpdated(newConstitution);
+    }
 
-    function addMember(address newMember) public onlyOwner {
-        require(!members[newMember], "Address is already a member.");
-        members[newMember] = true;
-        emit MemberAdded(newMember);
-    }
+    function addMember(address newMember) public onlyOwner {
+        require(!members[newMember], "Address is already a member.");
+        members[newMember] = true;
+        memberList.push(newMember);
+        emit MemberAdded(newMember);
+    }
 
-    function removeMember(address member) public onlyOwner {
-        require(members[member], "Address is not a member.");
-        members[member] = false;
-        emit MemberRemoved(member);
-    }
+    function removeMember(address member) public onlyOwner {
+        require(members[member], "Address is not a member.");
+        members[member] = false;
+        emit MemberRemoved(member);
+    }
 
-    function isMember(address addr) public view returns (bool) {
-        return members[addr];
-    }
+    function isMember(address addr) public view returns (bool) {
+        return members[addr];
+    }
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "New owner is the zero address.");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address.");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 
-    function getOwner() public view returns (address) {
-        return owner;
-    }
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    // New Functions Below
+
+    function getConstitutionHistory() public view returns (string[] memory) {
+        return constitutionHistory;
+    }
+
+    function getTotalMembers() public view returns (uint256) {
+        return memberList.length;
+    }
+
+    function getMembers(uint256 startIndex, uint256 count) public view returns (address[] memory) {
+        require(startIndex < memberList.length, "Start index out of range");
+
+        uint256 end = startIndex + count;
+        if (end > memberList.length) {
+            end = memberList.length;
+        }
+
+        address[] memory result = new address[](end - startIndex);
+        for (uint256 i = startIndex; i < end; i++) {
+            result[i - startIndex] = memberList[i];
+        }
+
+        return result;
+    }
 }
-
-
